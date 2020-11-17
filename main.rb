6dedc79b979d2974e72df94535b32d7944b52b06
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-11-18 07:38:17 +0900

    :recycle: requireを一番上に持ってくる

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
+require 'net/http'
+require 'json'
+
 res = []
-ARGV.each do |arg|
-  require 'net/http'
+ARGV.each do |arg|  
   a = Net::HTTP.get(URI.parse('https://gasyuku-api.herokuapp.com/users/' + arg))
-  require 'json'
   a = JSON.parse(a)
   a['subscribed_shop_ids'].each do |id|
     b = Net::HTTP.get(URI.parse('https://gasyuku-api.herokuapp.com/shops/' + id.to_s))
